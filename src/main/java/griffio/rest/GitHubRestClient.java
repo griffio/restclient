5b61e398ed4bab724aedbeb69ccaa7b48cb07712
griffio <griffio@users.noreply.github.com>
2015-05-17 12:05:31 +0100

    Request Interceptor

--- src/main/java/griffio/rest/GitHubRestClient.java
+++ src/main/java/griffio/rest/GitHubRestClient.java
@@ -3,6 +3,7 @@ package griffio.rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import griffio.gson.GitHubUserJsonDeserializer;
+import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.converter.GsonConverter;
 
@@ -12,11 +13,19 @@ public class GitHubRestClient {
 
     public GitHubRestClient() {
 
+      final RequestInterceptor requestInterceptor = new RequestInterceptor() {
+        @Override
+        public void intercept(RequestInterceptor.RequestFacade request) {
+          request.addHeader("Accept", "application/vnd.github.v3+json");
+        }
+      };
+
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(GitHubUser.class, new GitHubUserJsonDeserializer())
                 .create();
 
         RestAdapter restAdapter = new RestAdapter.Builder()
+                .setRequestInterceptor(requestInterceptor)
                 .setEndpoint("https://api.github.com")
                 .setConverter(new GsonConverter(gson))
                 .build();
