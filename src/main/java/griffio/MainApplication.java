cfb4fddaafe1e36a0f5184db6a64b6f6e4097034
griffio <griffio@users.noreply.github.com>
2015-12-20 20:06:51 +0000

    retrofit 2.0.0-beta2

--- src/main/java/griffio/MainApplication.java
+++ src/main/java/griffio/MainApplication.java
@@ -1,19 +1,18 @@
 package griffio;
 
+import griffio.rest.GitHubApiService;
 import griffio.rest.GitHubRestClient;
 import griffio.rest.GitHubUser;
 import retrofit.Call;
 
+import java.io.IOException;
+
 public class MainApplication {
 
-    public static void main(String[] args) {
-
-        String username = args.length != 0 ? args[0] : "griffio";
-
-        Call<GitHubUser> user = new GitHubRestClient().getGitHubApiService().user(username);
-
-        System.out.println(user);
-
-    }
-
+  public static void main(String[] args) throws IOException {
+    String username = args.length != 0 ? args[0] : "griffio";
+    GitHubApiService api = new GitHubRestClient().getGitHubApiService();
+    Call<GitHubUser> user = api.user(username);
+    System.out.println(user.execute().body().name()); //synchronous
+  }
 }
