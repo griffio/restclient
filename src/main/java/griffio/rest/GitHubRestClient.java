99764ff65ee76d438304994ca0d528c03390d153
griffio <griffio@users.noreply.github.com>
2016-02-05 20:02:33 +0000

    2.0.0-beta4
    retrofit2.converter.gson.GsonConverterFactory

--- src/main/java/griffio/rest/GitHubRestClient.java
+++ src/main/java/griffio/rest/GitHubRestClient.java
@@ -7,7 +7,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import griffio.gson.GitHubUserJsonDeserializer;
-import retrofit2.GsonConverterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Retrofit;
 
 import java.io.IOException;
